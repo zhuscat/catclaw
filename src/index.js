895195a0d8fc2e5b73abb18150e5cb325019a8c7
zhuscat <zhudx6512@163.com>
2017-06-18 11:51:19 +0800

    :wrench: general BUG fix

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const utils = require('./utils');
 const userAgents = require('./userAgents.json');
 const iconv = require('iconv-lite');
 
+let timeoutId = null;
+
 /*
 options
 - startUrls
@@ -37,7 +39,11 @@ function CatClaw(options) {
   this.drain = options.drain;
   // 可以替换 seen，只要接口一致
   this.seen = options.seen || new SimpleSeen();
-  this.seenEnable = options.seenEnable || true;
+  if (typeof options.seenEnable === 'boolean') {
+    this.seenEnable = options.seenEnable;
+  } else {
+    this.seenEnable = true;
+  }
   this.hostsRestricted = options.hostsRestricted;
   this.referer = options.referer;
   this.encoding = options.encoding || 'utf8';
@@ -48,12 +54,10 @@ CatClaw.prototype.start = function start() {
     const urlItem = this._queue.dequeue();
     this.request(urlItem);
     this._concurrency = this._concurrency + 1;
-    setTimeout(() => {
+    timeoutId = setTimeout(() => {
       this.start();
+      timeoutId = null;
     }, this.interval);
-  }
-  if ((this._concurrency === 0) && (this._queue.getLen() === 0)) {
-    this.drain();
   }
 };
 
@@ -72,6 +76,7 @@ CatClaw.prototype.request = function myrequest(urlItem) {
   }
   request(requestOptions, (err, res) => {
     let callback, $;
+    const previousQueueLen = this._queue.getLen();
     for (let i = 0; i < this.routes.length; i++) {
       const route = this.routes[i];
       if (route[0].test(url)) {
@@ -85,6 +90,8 @@ CatClaw.prototype.request = function myrequest(urlItem) {
     if (err) {
       urlItem.retries = urlItem.retries + 1;
       if (urlItem.retries < this.maxRetries) {
+        // 这是直接往队列里面加
+        console.log(`[INFO] retry ${url} retry times: ${urlItem.retries}`);
         this._queue.enqueue(urlItem);
       }
     } else if (res.body) {
@@ -97,9 +104,22 @@ CatClaw.prototype.request = function myrequest(urlItem) {
     });
     if (this._concurrency === this.maxConcurrency) {
       this._concurrency = this._concurrency - 1;
-      this._eventEmitter.emit('available');
+      // 只有在 timeoutId 为 null 的时候发送 available
+      // 因为不为 null 的话，之后会调用 start 函数
+      if (timeoutId === null) {
+        this._eventEmitter.emit('available');
+      }
     } else {
-      this._concurrency = this._concurrency - 1;
+      if (previousQueueLen === 0 && this._queue.getLen() > 0 && timeoutId === null) {
+        this._concurrency = this._concurrency - 1;
+        this._eventEmitter.emit('available');
+      } else {
+        this._concurrency = this._concurrency - 1;
+      }
+    }
+    // 一个请求完成之后，如果并发量和队列都为0的话，就调用 drain
+    if (this._concurrency === 0 && this._queue.getLen() === 0) {
+      this.drain();
     }
   });
 }
@@ -123,11 +143,17 @@ CatClaw.prototype.add = function add(fromUrl, toUrlOption) {
   // now it's simply judge if two url are the same (not compare request `body`, `method` etc.)
   if (this.seenEnable) {
     if (this.seen.add(encodedUrl)) {
+      console.log(`[INFO] skip duplicate url: ${encodedUrl}`);
       this._queue.enqueue({
         url: encodedUrl,
         retries: 0,
       });
     }
+  } else {
+    this._queue.enqueue({
+      url: encodedUrl,
+      retries: 0,
+    });
   }
 };
 
